baf70c4b78d9ad20dd2666d71d3f1dae6f7bc81b
SwordAndTea <xiangweiqaz@gmail.com>
2025-11-21 15:30:28 -0500

    add switches

---
+++ Shadow_Hu/Assets/Scripts/Switch/AbstractSwitch.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Switch
+{
+    public abstract class AbstractSwitch : MonoBehaviour
+    {
+        public UnityEvent<bool> onActivateStateChange = new ();
+        
+        public bool IsActivated { get; protected set; }
+        
+        public abstract void Switch();
+
+        protected void OnTriggerEnter2D(Collider2D other)
+        {
+            Switch();
+        }
+    }
+}
